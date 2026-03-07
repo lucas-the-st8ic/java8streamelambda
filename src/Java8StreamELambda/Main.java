f8046d259b39a6f678f3ee8d9b0ff5102ec90878
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-07 18:48:00 -0300

    updates

--- src/Java8StreamELambda/Main.java
+++ src/Java8StreamELambda/Main.java
@@ -5,10 +5,39 @@ import java.util.List;
 
 public class Main {
     static void main(String[] args) {
-        List<Integer> lista = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        //Exemplos de Operações Lambda.
+
+        List<Integer> lista = Arrays.asList(1,2,3,4,5,6,6,7,8,9,9,10);
+
+/*        lista.stream()
+                .filter(x -> x % 2 == 0)
+                .forEach(System.out::println);
+        System.out.println("==============");
 
         lista.stream()
-                .filter(x -> x % 2 == 0)
+                .skip(2)
+                .forEach(System.out::println);
+
+        System.out.println("==============");
+        lista.stream()
+                .limit(2)
+                .forEach(System.out::println);
+
+        System.out.println("==============");
+        lista.stream()
+                .skip(2)
+                .limit(6)
+                .distinct() // equals e hashcode
+                .forEach(System.out::println);
+
+        System.out.println("==============");
+        lista.stream()
+                .filter(e -> e % 2 == 0)
+                .forEach(System.out::println);*/
+
+        System.out.println("==============");
+        lista.stream()
+                .map(n -> n * 2)
                 .forEach(System.out::println);
     }
 }
