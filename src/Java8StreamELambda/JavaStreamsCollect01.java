be1260f9b70f935b7944ec6bad186f6e96d4912f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-15 16:40:06 -0300

    Update JavaStreamsCollect01.java

--- src/Java8StreamELambda/JavaStreamsCollect01.java
+++ src/Java8StreamELambda/JavaStreamsCollect01.java
@@ -1,8 +1,6 @@
 package Java8StreamELambda;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class JavaStreamsCollect01 {
@@ -41,5 +39,60 @@ public class JavaStreamsCollect01 {
                 .collect(Collectors.summingInt(Integer::intValue));
 
         System.out.println(soma);
+
+
+        //summarizing
+        IntSummaryStatistics stats = lista.stream()
+                .collect(Collectors.summarizingInt(Integer::intValue));
+
+        System.out.println(stats.getMin());
+        System.out.println(stats.getMax());
+        System.out.println(stats.getCount());
+        System.out.println(stats.getSum());
+        System.out.println(stats.getAverage());
+
+
+        //counting
+
+        Long contagem = lista.stream()
+                .filter(i -> i % 2 == 0)
+                .collect(Collectors.counting());
+
+        System.out.println(contagem);
+
+        //Max/Min
+
+        Optional<Integer> max= lista.stream()
+                .max(Comparator.naturalOrder());
+
+        System.out.println("Maior: " + max.get());
+
+        Optional<Integer> min= lista.stream()
+                .max(Comparator.naturalOrder());
+
+        System.out.println("Menor: " + min.get());
+
+        //groupingBy
+
+        Map<Integer, List<Integer>> mapa = lista.stream()
+                .collect(Collectors.groupingBy(n -> n % 3));
+
+        System.out.println(mapa);
+
+        //partitioninBy
+
+        Map<Boolean, List<Integer>> partition = lista.stream()
+                .collect(Collectors.partitioningBy(n -> n % 3 == 0));
+
+        System.out.println(partition);
+        
+        //tomap
+
+        Map<Integer, Double> mapeador = lista.stream()
+                .collect(Collectors.toMap(c -> c,
+                        n -> Math.pow(n.doubleValue(), 5)));
+
+        System.out.println(mapeador);
+        
     }
 }
