de888fe0a96206e3812a3814f9dd653f66dbb106
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-21 12:44:37 -0300

    Update DebugLambda.java

--- src/Java8StreamELambda/DebugLambda.java
+++ src/Java8StreamELambda/DebugLambda.java
@@ -9,14 +9,19 @@ public class DebugLambda {
         List<Integer> listaNumeros = Arrays.asList(1, 2, 5, 8, 9, 15, 33, 67, 90);
 
         listaNumeros.stream()
-                .map(n -> {
-                    StringBuilder builder = new StringBuilder();
-                    builder.append(n);
-                    builder.append("s");
-                    builder.append("a");
-                    return builder;
-                } )
+                .peek(n -> System.out.println(n + " Se inscreva no canal!"))
+                .filter(n -> n % 2 == 0)
+                .peek(n -> System.out.println(n + " Comente"))
+                .map(n -> getStringBuilder(n))
                 .forEach(System.out::println);
-        
+
+    }
+
+    private static StringBuilder getStringBuilder(Integer n) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(n);
+        builder.append("s");
+        builder.append("a");
+        return builder;
     }
 }
