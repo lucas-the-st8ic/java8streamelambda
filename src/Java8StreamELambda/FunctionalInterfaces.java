c7bc579f75bd87df002025654d3466cf8bb92c8c
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-05 18:15:49 -0300

    Update FunctionalInterfaces.java

--- src/Java8StreamELambda/FunctionalInterfaces.java
+++ src/Java8StreamELambda/FunctionalInterfaces.java
@@ -8,7 +8,7 @@ public class FunctionalInterfaces {
     static void main(String[] args) {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5);
 
-        lista.stream()
-                .
+        lista.stream();
+
     }
 }
